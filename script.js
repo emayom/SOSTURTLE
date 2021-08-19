6c0289d8c57ee8594b94241d31547eae7c093e09
emayom <emayom@naver.com>
2021-08-19 18:17:55 +0900

    sosturtle #media-queries

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const page1 = document.getElementById("page-1");
 const page2 = document.getElementById("page-2");
 const page3 = document.getElementById("page-3");
 
+const amount = document.getElementById("amount");
+const period = document.getElementById("period");
+const value = document.getElementById("value");
+
+const donate = document.getElementById("donate-btn");
+const modal = document.getElementById("thanks");
+const modal_close = document.getElementById("modal-close");
+
 function handleMainImg(event) {
     const target = event.srcElement.id;
 
@@ -16,6 +24,22 @@ function handleMainImg(event) {
     }
 }
 
+const HIDDEN_CLASSNAME = "hidden";
+
+function openModal() {
+    value.innerHTML = `You donate ${amount.value}$ for a billion baby turtle. ${period.value}`;
+
+    modal.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function closeModal() {
+    modal.classList.add(HIDDEN_CLASSNAME);
+}
+
 page1.addEventListener("click", handleMainImg);
 page2.addEventListener("click", handleMainImg);
 page3.addEventListener("click", handleMainImg);
+
+donate.addEventListener("click", openModal);
+modal_close.addEventListener("click", closeModal);
+
