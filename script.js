f47acb62ac560f6d9fe04489eb7981e55c1d1376
emayom <emayom@naver.com>
2021-08-19 23:29:09 +0900

    sosturtle #add JS

--- script.js
+++ script.js
@@ -9,31 +9,74 @@ const period = document.getElementById("period");
 const value = document.getElementById("value");
 
 const donate = document.getElementById("donate-btn");
-const modal = document.getElementById("thanks");
+const modal = document.getElementById("thanks-bg");
 const modal_close = document.getElementById("modal-close");
+
+const email_input = document.getElementById("email-input");
+const email_submit = document.getElementById("email-btn");
+
+const IMG_SRC = "https://emayom.github.io/SOSTURTLE/img/";
+const IMG = ["main_img.png", "main_img2.jpg", "main_img3.jpeg"];
 
 function handleMainImg(event) {
     const target = event.srcElement.id;
 
     if(target === "page-1") {
-        mainImg.src = "https://emayom.github.io/SOSTURTLE/img/main_img.png"
+        mainImg.src = `${IMG_SRC}${IMG[0]}`;
     } else if(target === "page-2") {
-        mainImg.src = "https://emayom.github.io/SOSTURTLE/img/main_img2.jpg"
+        mainImg.src = `${IMG_SRC}${IMG[1]}`;
     } else if (target === "page-3"){
-        mainImg.src = "https://emayom.github.io/SOSTURTLE/img/main_img3.jpeg"
+        mainImg.src = `${IMG_SRC}${IMG[2]}`;
     }
 }
 
 const HIDDEN_CLASSNAME = "hidden";
 
 function openModal() {
-    value.innerHTML = `You donate ${amount.value}$ for a billion baby turtle. ${period.value}`;
+
+    let pd;
+
+    switch(period.value) {
+        case "Annually":
+            pd = "year";
+            break;
+        case  "Monthly":
+            pd = "month";
+            break;
+        case "Weekly":
+            pd = "week";
+            break;
+        default:
+            pd = "day";
+            break;
+    }
+
+    value.innerHTML = `${amount.value}.00 every ${pd}`;
 
     modal.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function closeModal() {
     modal.classList.add(HIDDEN_CLASSNAME);
+}
+
+const emailValidation = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+
+function handleEmailSubmit(event) {
+    event.preventDefault();
+
+    if(!email_input.value == ""){
+        if(isValid(email_input.value)){
+            email_input.value = "";
+        } else {
+            alert("Email is not valid.");
+            email_input.value = "";
+        }
+    }
+}
+
+function isValid(email) {
+    return emailValidation.test(email);
 }
 
 page1.addEventListener("click", handleMainImg);
@@ -43,3 +86,4 @@ page3.addEventListener("click", handleMainImg);
 donate.addEventListener("click", openModal);
 modal_close.addEventListener("click", closeModal);
 
+email_submit.addEventListener("click", handleEmailSubmit);
